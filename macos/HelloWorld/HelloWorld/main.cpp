03698cdadb493c1b07b30e8641c7db61522ee411
Youngbeom Kim <bluewings109@gmail.com>
2019-03-17 15:25:27 +0900

    First commit on macOS

---
+++ macos/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by 김영범 on 17/03/2019.
+//  Copyright © 2019 김영범. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
